f77711aeae24495c73fac21b9ac836914a82fdcf
8ik3l <8ik3l1@gmail.com>
2026-09-20 16:28:32 +0000

    Prepare Expo Video Studio: app/_layout.js

--- app/_layout.js
+++ app/_layout.js
@@ -1,2 +1,1 @@
-import {Stack} from 'expo-router';import {StatusBar} from 'expo-status-bar';import {StudioProvider} from '../context/StudioContext';
-export default function Layout(){return <StudioProvider><StatusBar style="light"/><Stack screenOptions={{headerShown:false,contentStyle:{backgroundColor:'#070A0F'}}}/></StudioProvider>}
+import{Stack}from'expo-router';import{StatusBar}from'expo-status-bar';import{StudioProvider}from'../context/StudioContext';export default function Layout(){return <StudioProvider><StatusBar style="light"/><Stack screenOptions={{headerShown:false,contentStyle:{backgroundColor:'#070A0F'}}}/></StudioProvider>}
